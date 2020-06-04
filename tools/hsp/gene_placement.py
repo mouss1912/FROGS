5d433cba8cc659f857532c1ba1f7796aeb198180
moussa <moussasamb1912@gmail.com>
2020-06-04 10:39:57 +0200

    hsp avec categorie v3

--- tools/hsp/gene_placement.py
+++ tools/hsp/gene_placement.py
@@ -15,6 +15,11 @@ import json
 import re
 from numpy import median
 
+#import argparse
+#from picrust2.wrap_hsp import castor_hsp_workflow
+#from picrust2.util import make_output_dir_for_file, check_files_exist
+#from picrust2.default import default_tables
+
 CURRENT_DIR = os.path.dirname(os.path.abspath(__file__))
 # PATH: executable
 BIN_DIR = os.path.abspath(os.path.join(os.path.dirname(CURRENT_DIR), "libexec"))
@@ -40,23 +45,27 @@ from frogsBiom import BiomIO
 
 #
 ##################################################################################################################################################
-class hsp_16S(Cmd):
+#os.system("hsp.py "+ hsp_16_Met  +" --in_trait "+ str(in_trait) +" --tree "+ str(tree) +" --output "+ str(output) +" --calculate_NSTI"+str(calculate_NSTI))
+
+class hsp16S(Cmd):
     """
     @summary: PICRUSt2 (Phylogenetic Investigation of Communities by Reconstruction of Unobserved States)
     @summary: hsp.py program  predict number copie of gene family for each OTU.
     @summary: use 16S, EC and/or KO 
     @see: https://github.com/picrust/picrust2/wiki
     """
-
-    def __init__(self, hsp16Met, in_trait, tree, output, calculate_NSTI, stdout):
-       
+    
+    def __init__(self, hsp16Met,categorie, in_trait, tree, output, n, stdout):
+    #def __init__(self, hsp16Met, categorie, in_trait, tree, output, n, stdout):    
+       #os.system("hsp.py "+ hsp16Met  +" -i "+ str(in_trait) +" -t "+ str(tree) +" -o "+ str(output) +" --observed_trait_table "+ str(observed_trait_table) + " -n " )
+        #-n: Calculate NSTI and add to output file.
         Cmd.__init__(self,
                  'hsp.py',
                  'predict gene copy 16S', 
-                  "" + hsp_16_Met  +" --in_trait "+ str(in_trait) +" --tree "+ str(tree) +" --output "+ str(output) +" --calculate_NSTI"+str(calculate_NSTI) +' 2> ' + stdout,
+                  "" + hsp16SMet  +" -i "+ str(in_trait) +" -t "+ str(tree) +" -o "+ str(output) +" -n" +' 2> ' + stdout,
                 "--version") 
       
-
+        
     def get_version(self):
         """
         @summary: Returns the program version number.
@@ -67,7 +76,7 @@ class hsp_16S(Cmd):
         
 
 #####################################################   
-class hsp_EC(Cmd):
+class hspITS(Cmd):
     """
     @summary: PICRUSt2 (Phylogenetic Investigation of Communities by Reconstruction of Unobserved States)
     @summary: hsp.py program  predict number copie of gene family for each OTU.
@@ -75,15 +84,17 @@ class hsp_EC(Cmd):
     @see: https://github.com/picrust/picrust2/wiki
     @commande: hsp.py -i EC -t placed_seqs.tre -o EC_predicted.tsv.gz -p 1
     """
-    def __init__(self, hspECMet, in_trait, tree, output, stdout):
+    def __init__(self, hspITSMet,categorie, tree, output, observed_trait_table, n, stdout):
+        #os.system("hsp.py "+ hsp_EC_Met  +" -i "+ str(in_trait) +" -t "+ str(tree) +" -o "+ str(output))
+        #os.system("hsp.py "+ hspITSMet+ " -t "+ str(tree) +" -o "+ str(output) +" --observed_trait_table "+ str(observed) + " -n ")
+
        
         Cmd.__init__(self,
                  'hsp.py',
-                 'predict gene copy 16S', 
-                  "" + hsp_EC_Met  +" --in_trait "+ str(in_trait) +" --tree "+ str(tree) +" --output "+ str(output) +' 2> ' + stdout,
+                 'predict gene copy fungi', 
+                  "" + hspITSMet  +" -t "+ str(tree) +" -o "+ str(output) + " --observed_trait_table "+ str(observed_trait_table) +" -n" +' 2> ' + stdout,
                 "--version") 
-        
-
+    
     def get_version(self): 
         """
         @summary: Returns the program version number.
@@ -92,45 +103,81 @@ class hsp_EC(Cmd):
 
         return Cmd.get_version(self, 'stdout').split()[1].strip() 
 
-       
-#################################################################
-class hsp_KO(Cmd):
-    """
-    @summary: PICRUSt2 (Phylogenetic Investigation of Communities by Reconstruction of Unobserved States)
-    @summary: hsp.py program  predict number copie of gene family for each OTU.
-    @summary: use KO 
-    @see: https://github.com/picrust/picrust2/wiki
-    @commande: hsp.py -i KO -t placed_seqs.tre -o KO_predicted.tsv.gz -p 1
-    """
-    def __init__(self, hspKOMet, in_trait, tree, output, stdout):
-       
-        Cmd.__init__(self,
-                 'hsp.py',
-                 'predict gene copy 16S', 
-                  "" + hsp_KO_Met  +" --in_trait "+ str(in_trait) +" --tree "+ str(tree) +" --output "+ str(output) +' 2> ' + stdout,
-                "--version") 
-        
 
-    def get_version(self): 
-        """
-        @summary: Returns the program version number.
-        @return: [str] Version number if this is possible, otherwise this method return 'unknown'.
-        """
 
-        return Cmd.get_version(self, 'stdout').split()[1].strip() 
-
-  
-    ########################
-    print("partie2")
 ##################################################################################################################################################
 #
 # FUNCTIONS
 #
 ##################################################################################################################################################
-    #Fonction restricted float
+def write_summary( summary_file, output, biom_file):
+    """
+    @summary: Writes the summary of results.
+    @param summary_file: [str] The output file.
+    @param results_chimera: [str] Path to the input chimera step summary.
+    """
+    # Get data
+    detection_categories = ["sequence", "16S_rRNA_Count"]
+    detection_data = list()
+    remove_data = dict()
+    """
+    # Parse results chimera
+    in_remove_metrics = True
+    in_detection_metrics = False
+    section_first_line = True
+    log_fh = open(output)
+    
+    for line in log_fh:
+        line = line.strip()
+        if line.startswith('##Metrics by sample'):
+            remove_metrics = False
+            in_detection_metrics = True
+            section_first_line = True
+        elif line.startswith('##Metrics global'):
+            remove_metrics = True
+            in_detection_metrics = False
+            section_first_line = True
+        elif line == "":
+            in_detection_metrics = False
+            in_remove_metrics = False
+        else:
+            if in_detection_metrics:
+                if section_first_line:
+                    line_fields = line[1:].split("\t")[1:]
+                    detection_categories = line_fields
+                    section_first_line = False
+                else:
+                    line_fields = line.split("\t")
+                    detection_data.append({
+                             'name': line_fields[0],
+                             'data': map(int, line_fields[1:])
+                    })
+            elif in_remove_metrics:
+                if section_first_line:
+                    line_fields = line[1:].split("\t")
+                    remove_categories = [category.lower().replace(" ", "_") for category in line_fields]
+                    section_first_line = False
+                else:
+                    for idx, val in enumerate(line.split("\t")):
+                        remove_data[remove_categories[idx]] = int(val)
+    
+    log_fh.close()
+    """
+    # Write
+    FH_summary_tpl = open( os.path.join(CURRENT_DIR, "gene_placement.html") )
+    FH_summary_out = open( summary_file, "w" )
+    for line in FH_summary_tpl:
+        if "###DETECTION_CATEGORIES###" in line:
+            line = line.replace( "###DETECTION_CATEGORIES###", json.dumps(detection_categories) )
+        elif "###DETECTION_DATA###" in line:
+            line = line.replace( "###DETECTION_DATA###", json.dumps(detection_data) )
+        elif "###REMOVE_DATA###" in line:
+            line = line.replace( "###REMOVE_DATA###", json.dumps(remove_data) )
+        print(line)
+        FH_summary_out.write( line )
 
-################################
-print("partie3")
+    FH_summary_out.close()
+    FH_summary_tpl.close()
 ##################################################################################################################################################
 #
 # MAIN
@@ -138,25 +185,65 @@ print("partie3")
 ##################################################################################################################################################
 
 if __name__ == "__main__":
+    #Categorie
+    Categorie = ['16S', 'ITS', '18S']
     # Table to use for prediction
     TRAIT_OPTIONS = ['16S', 'COG', 'EC', 'KO', 'PFAM', 'TIGRFAM', 'PHENO']
     # prediction method to use 
     HSP_METHODS = ['mp', 'emp_prob', 'pic', 'scp', 'subtree_average']
+    #Pour personaliser m
+    #retourne le couple de valeur: 1er indice , 2em la valeur
+    find = False
+    for i, v in enumerate(sys.argv):
+        if sys.argv[i] == "-c" or sys.argv[i] == "--categorie":
+            if sys.argv[i+1] == "16S":
+                find = True      
 
     # Manage parameters
     parser = argparse.ArgumentParser( description='predict gene family for OTU' )
 
-
+    print("partie4")
     # Inputs
+    #Les inputs categoriel
     group_input = parser.add_argument_group( 'Inputs' )
 
-    group_input.add_argument('-i', '--in_trait', type=str.upper, choices=TRAIT_OPTIONS, help='Specifies which default trait table should be used. Use the --observed_trait_table option to input a non-default trait table.')
+    group_input.add_argument('-c', '--categorie',choices=Categorie, help='Specifies which categorie 16S or ITS, 18S')
+
+    if find:
+        group_input.add_argument('-i', '--in_trait',choices=TRAIT_OPTIONS,  help='Specifies which default trait table should be used. Use the --observed_trait_table option to input a non-default trait table.')
+
+    else:
+        group_input.add_argument('-i', '--observed_trait_table', metavar='PATH', type=str, help='The input trait table describing directly observed traits (e.g. sequenced genomes) in tab-delimited format. Necessary if you want to use a custom table.')
+
+    #Pour avoir plusieur parametres   
+    # tab_arg = []
+    # for i, v in enumerate(sys.argv):
+    #      if sys.argv[i] == "-i":
+    #         ind = 0
+    #         for e in range(i+1, len(sys.argv)):
+    #             if sys.argv[e][0] == "-" :
+    #                 ind = e
+    #                 print(i+1, ind, len(sys.argv), sys.argv[e])
+    #                 break
+
+    #         if ind != 0:
+    #             tab_arg = sys.argv[i+1:ind]
+    #             print(tab_arg)
+    #             break
+
+    #group_input.add_argument('-t', '--table', help='Specifies which table choices, ')
+
+    #les parametres intermediares
+    #group_input.add_argument('-i', '--in_trait', type=str.upper, choices=TRAIT_OPTIONS, help='Specifies which default trait table should be used. Use the --observed_trait_table option to input a non-default trait table.')
+    #group_input.add_argument('-i', '--in_trait',  help='Specifies which default trait table should be used. Use the --observed_trait_table option to input a non-default trait table.')
 
     group_input.add_argument('-t', '--tree', metavar='PATH', required=True, type=str, help='The full reference tree in newick format containing both study sequences (i.e. ASVs or OTUs) and reference sequences.')
 
     group_input.add_argument('-o', '--output', metavar='PATH', type=str, required=True, help='Output table with predicted abundances per study sequence in input tree. If the extension \".gz\" is added the table will automatically be gzipped.')
 
-    group_input.add_argument('--observed_trait_table', metavar='PATH', type=str, help='The input trait table describing directly observed traits (e.g. sequenced genomes) in tab-delimited format. Necessary if you want to use a custom table.')
+    #group_input.add_argument('--observed', metavar='PATH', type=str, help='The input trait table describing directly observed traits (e.g. sequenced genomes) in tab-delimited format. Necessary if you want to use a custom table.')
+    #group_input.add_argument('--observed_trait_table', metavar='PATH', type=str, help='The input trait table describing directly observed traits (e.g. sequenced genomes) in tab-delimited format. Necessary if you want to use a custom table.')
+
 
     group_input.add_argument('--chunk_size', default=500, type=int, help='Number of functions to run at a time on one processor. Note that you should consider how many ''processes you have specified before changing this ''option. E.g. if you specify the chunk_size to be ''the total number of functions, 1 processor will ''be used even if you specified more so the job will ''be substantially slower (default: %(default)d).')
 
@@ -166,14 +253,17 @@ if __name__ == "__main__":
 
     group_input.add_argument('--verbose', default=False, action='store_true',help='If specified, print out wrapped commands and other ''details to screen.')
 
-    #group_input.add_argument('-v', '--version', default=False, action='version', version="%(prog)s " + __version__)
+    group_input.add_argument('-v', '--version', default=False, action='version', version="%(prog)s " + __version__)
+
+    group_input.add_argument('-b', '--biom_file', metavar='PATH', required=True, type=str, help='Biom file.')
+
   
     # output
     group_output = parser.add_argument_group( 'Outputs' )
 
-    #group_output.add_argument('-m','--html', default='summary.html', help="Path to store resulting html file. [Default: %(default)s]" )    
+    group_output.add_argument('-s','--html', default='summary.html', help="Path to store resulting html file. [Default: %(default)s]" )    
 
-    group_output.add_argument('-n', '--calculate_NSTI', default=False, action='store_true', help='Calculate NSTI and add to output file.')
+    group_output.add_argument('-n', '--calculate', default=False, action='store_true', help='Calculate NSTI and add to output file.')
 
     group_output.add_argument('--check', default=False, action='store_true', help='Check input trait table before HSP.')
 
@@ -181,32 +271,46 @@ if __name__ == "__main__":
 
     group_output.add_argument( '-l', '--log-file', default=sys.stdout, help='This output file will contain several information on executed commands.')
 
-
+    print("partie5")
     args = parser.parse_args()
-    #######
-    stderr = "hsp.stderr"
-    ######
-    hsp_16_Met = ""
-    hsp_EC_Met = ""
-    hsp_KO_Met = ""
+    tab_arg = []
+    if find:
+        tab_arg = args.in_trait.split(",")
+    else :
+        tab_arg = args.observed_trait_table.split(",")
 
+    print(tab_arg)
+    ####Declarer la sortie d'erreur ###
+    stderr = "hsp.stderr"
+    ### Declarer les methodes hsp ###
+    hsp16SMet = ""
+    hspITSMet = ""
+    print("partie6")
     # Process 
     try:     
-        #~ print(default_ref_dir)
-        #~ print(restricted_float)
-        print("\n\n\n--------------")
-        #~ print(args)
-        os.system("pwd")
-        #####
-        hsp_16S(hsp_16_Met, args.in_trait, args.tree, args.calculate_NSTI, stderr).submit( args.log_file )
+        Logger.static_write(args.log_file, "## Application\nSoftware :" + sys.argv[0] + " (version : " + str(__version__) + ")\nCommand : " + " ".join(sys.argv) + "\n\n")
+        # Commands execution
+        print("partie7")
+        for i, v in enumerate(tab_arg):
+            if args.categorie == "16S":
+                    hsp16_cmd = hsp16S(hsp16SMet ,"16S", v, args.tree, v+"_"+args.output, args.calculate, stderr).submit(args.log_file)
+            else :
+                    hspITS_cmd = hspITS(hspITSMet, args.categorie, args.tree, "fichier_"+str(i)+"_"+args.output, v, args.calculate, stderr).submit(args.log_file)         
+              
+    
+        #hsp_16S(hsp_16_Met, args.in_trait, args.tree, args.output, args.observed_trait_table, args.calculate, stderr)
+        #hsp16S(hsp16SMet, args.in_trait, args.tree,args.output,args.calculate, stderr).submit(args.log_file)
         #
-        hsp_EC(hsp_EC_Met, args.in_trait, args.tree, stderr).submit( args.log_file )
-        #
-        hsp_KO(hsp_KO_Met, args.in_trait, args.tree, stderr).submit( args.log_file )
-        ########
-        print("Partie hsp fini")
-        ########
-        #write_summary( args.html, "sout.fasta", "excluded.tsv", args.biom_file, args.out_tree)
+        print("partie8")
+        #hsp_EC(hsp_EC_Met, args.tree, args.output, args.observed_trait_table, args.calculate, stderr)
+
+        
+        #hspITS(hspITSMet, args.tree, args.output,  args.observed_trait_table, args.calculate, stderr).submit(args.log_file)
+        #hspITS(hspITSMet, args.tree, args.output,  args.observed, args.calculate, stderr)
+
+        print("partie9")    
+        write_summary(args.html, "output", args.biom_file)
+
     finally:
         print("Partie Finale ")
 
